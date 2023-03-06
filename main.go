4babe73cb6dd75ca5e8ecdc9ad2bfee7db51dd40
Volkan Van <volkanvan@gmail.com>
2023-03-06 21:26:05 +1100

    add apis for accounts

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/vanvolkan/simplebank/api"
+	db "github.com/vanvolkan/simplebank/db/sqlc"
+	"github.com/vanvolkan/simplebank/util"
+)
+
+func main() {
+	config, err := util.Loadconfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
